5402c57fc10dde995d8d49103242c763a9ebe5d5
Shawn Smith <shawnsmith8809@gmail.com>
2020-12-28 09:08:38 -0700

    hooked configuration files

--- configure.sh
+++ configure.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ -d "${PWD}/config/" ]; then
+	rm -r config/
+	echo "cleaning '${PWD}/config/'"
+fi
+if [ -d "${PWD}/wallet/" ]; then
+	rm -r wallet/
+	echo "cleaning '${PWD}/wallet/'"
+fi
 mkdir wallet
 mkdir config
 mkdir config/web
+original_dir=${PWD}
+cd scripts/
+./configureElectrum.sh
+
