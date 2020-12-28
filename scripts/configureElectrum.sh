3d208e02fba5a3f9856ad877f7bd29c55832e8f3
Shawn Smith <shawnsmith8809@gmail.com>
2020-12-28 01:40:01 -0700

    merged mac and linux config

--- scripts/configureElectrum.sh
+++ scripts/configureElectrum.sh
@@ -1,18 +1,39 @@
-if ! type "electrum" > /dev/null; then
-	echo "electrum not installed"
-else
-	if [ -z ${WALLET_NAME+x} ]; then WALLET_NAME="default"; else echo "wallet is set to '$WALLET_NAME'"; fi
-	if [ -z ${WALLET_PASSWORD+x} ]; then
-		echo "Using default password"; WALLET_PASSWORD="ourBoiLovesHisIceCream"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	if ! type "electrum" > /dev/null; then
+		echo "electrum not installed"
+	else
+		if [ -z ${WALLET_NAME+x} ]; then WALLET_NAME="default"; else echo "wallet is set to '$WALLET_NAME'"; fi
+		if [ -z ${WALLET_PASSWORD+x} ]; then
+			echo "Using default password"; WALLET_PASSWORD="ourBoiLovesHisIceCream"
+		fi
+		echo "starting electrum daemon"
+		electrum daemon -d
+		echo "set electrum rpc port 7777:"
+		electrum setconfig rpcport 7777
+		cd ..
+		new_path=${PWD}/wallet/${WALLET_NAME}
+		cd scripts/
+		echo "set wallet path ${new_path}:"
+		electrum setconfig wallet_path $new_path
 	fi
-	echo "starting electrum daemon"
-	electrum daemon -d
-	echo "set electrum rpc port 7777:"
-	electrum setconfig rpcport 7777
-	cd ..
-	new_path=${PWD}/wallet/${WALLET_NAME}
-	cd scripts/
-	echo "set wallet path ${new_path}:"
-	electrum setconfig wallet_path $new_path
-	python lib/generateElectrumWallet.py ${WALLET_PASSWORD}
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+	if [ -f "/Applications/Electrum.app/Contents/MacOS/run_electrum" ]; then
+		if [ -z ${WALLET_NAME+x} ]; then WALLET_NAME="default"; else echo "wallet is set to '$WALLET_NAME'"; fi
+		if [ -z ${WALLET_PASSWORD+x} ]; then
+			echo "Using default password"; WALLET_PASSWORD="ourBoiLovesHisIceCream"
+		fi
+		echo "starting electrum daemon"
+		/Applications/Electrum.app/Contents/MacOS/run_electrum daemon -d
+		echo "set electrum rpc port 7777:"
+		/Applications/Electrum.app/Contents/MacOS/run_electrum setconfig rpcport 7777
+		cd ..
+		new_path=${PWD}/wallet/${WALLET_NAME}
+		cd scripts/
+		echo "set wallet path ${new_path}:"
+		/Applications/Electrum.app/Contents/MacOS/run_electrum setconfig wallet_path $new_path
+	else
+		echo "electrum not installed"
+	fi
 fi
+python lib/generateElectrumWallet.py ${WALLET_PASSWORD}
--- scripts/lib/generateElectrumWallet.py
+++ scripts/lib/generateElectrumWallet.py
@@ -2,12 +2,18 @@ import sys
 import subprocess
 import json
 import os
+import platform
+
+if platform.system() == 'Linux':
+    electrum = 'electrum'
+elif platform.system() == 'Darwin':
+    electrum = "/Applications/Electrum.app/Contents/MacOS/run_electrum"
 
 def main():
     if (len(sys.argv) != 2):
         print("ERROR: Incorrect set of arguements for generateElectrumWallet.py. 1 password arguement is required")
         return
-    wallet_path = subprocess.check_output(["electrum", "getconfig", "wallet_path"])
+    wallet_path = subprocess.check_output([electrum, "getconfig", "wallet_path"])
     wallet_path = wallet_path[:len(wallet_path)-1]
     wallet = createWallet(wallet_path, sys.argv[1])
     writeConfig(wallet_path, sys.argv[1])
@@ -15,11 +21,11 @@ def main():
     load_wallet(wallet_path, sys.argv[1])
 
 def load_wallet(wallet_path, wallet_password):
-    subprocess.check_output(['electrum', 'load_wallet', '-w', wallet_path, '-W', wallet_password])
+    subprocess.check_output([electrum, 'load_wallet', '-w', wallet_path, '-W', wallet_password])
     
 def writeConfig(wallet_path, wallet_password):
-    user = subprocess.check_output(["electrum", "getconfig", "rpcuser"]).strip()
-    password = subprocess.check_output(["electrum", "getconfig", "rpcpassword"]).strip()
+    user = subprocess.check_output([electrum, "getconfig", "rpcuser"]).strip()
+    password = subprocess.check_output([electrum, "getconfig", "rpcpassword"]).strip()
     file = open(os.path.join(os.getcwd(), "../config/web/bitcoin_config.py"), "w+")
     file.write("bitcoin_config = {\n")
     if os.environ.get("ELECTRUM_HOSTNAME"):
@@ -38,7 +44,7 @@ def writeSensitive(walletOutput):
 
 def createWallet(path, password):
     print("wallet created at " + path.decode("utf-8"))
-    tmp = load_subprocess(["electrum", "create", "-w", path, "-W", password])
+    tmp = load_subprocess([electrum, "create", "-w", path, "-W", password])
     return tmp
 
 def load_subprocess(commands):
